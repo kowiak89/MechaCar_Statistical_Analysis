02611561b6e152da054e773fa99f970d3a6f6bec
David Szymkowiak <davidszymkowiak@Davids-MA.local>
2021-10-08 14:11:14 -0700

    delievarable 3 rscript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,3 +19,24 @@ head(total_summary)
 # Create a group_by(lot) for the same variables as above
 lot_summary = coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 head(lot_summary)
+
+##### Deliverable 3 #########
+
+# Set the population mean to 1500
+pop_mean <- 1500
+
+# Perform a t.test based off of pop_mean and all of the lots
+t.test(coils$PSI, mu=pop_mean)
+
+# Perform a t.test based off of pop_mean and each lot separately
+# Lot 1:
+lot1 <- subset(coils, Manufacturing_Lot == "Lot1")
+t.test(lot1$PSI, mu=pop_mean)
+
+# Lot 2:
+lot2 <- subset(coils, Manufacturing_Lot == "Lot2")
+t.test(lot2$PSI, mu=pop_mean)
+
+#Lot 3:
+lot3 <- subset(coils, Manufacturing_Lot == "Lot3")
+t.test(lot3$PSI, mu=pop_mean)
