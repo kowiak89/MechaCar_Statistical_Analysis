42a6583e791eb7c9ef0deaed4fb382d838b1e86a
David Szymkowiak <davidszymkowiak@Davids-MA.local>
2021-10-08 13:43:11 -0700

    deliverable 2 rscript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,17 @@ head(mecha_car)
 # Perform linear regression using all six variables
 lr <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car)
 summary(lr)
+
+#### Deliverable 2 ########
+
+# Read in the csv and check the data
+coils = read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+head(coils)
+
+# Create a total_summary variable that summarizes mean, median, variance, and sd for PSI
+total_summary = coils %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+head(total_summary)
+
+# Create a group_by(lot) for the same variables as above
+lot_summary = coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+head(lot_summary)
